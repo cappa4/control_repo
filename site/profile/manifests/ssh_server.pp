5c5f7091e2b88210b4e5c393247c895135cfe7e3
Marco <38658892+cappa4@users.noreply.github.com>
2018-04-24 15:44:39 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDZmzAGY0M8I95qJjT6jX+vhzWEG4+SNRQ8jQy6XnxWlk9ii8TwmlKCFiH5M2YZ7MVqGt3fip8PrtDesyHGFuaJuXtpm69qP8Y3VAsTyCHBK5SRcU9hzFYSgNh5OCsr7TA7WaIrC0r3bDTTsplK8qeDG4a1Jh7mP79nYM/UeJ2yAg6nEQJLr49ut0pSWG3a8VuukYIEBmuG81tDYoeG5e3LsOgzuIzTxJXeIwijfFXdTk7E3f3vPtxpAkYBSWHT5BDDYUBmL6+StYTVl3PMilr5nkwyZCL8U1J/J3BoyTdt9b9N+/YYJfO5JkAtty74IM5L0nc5d70i2LhRfV5zGslx',
 	}  
 }
